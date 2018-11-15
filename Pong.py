7fe00efac3f9206c6010b151d88a5ec0ce59970a
jcruse2 <44611075+jcruse2@users.noreply.github.com>
2018-11-15 01:17:23 -0500

    Create Pong.py

---
+++ Pong.py
@@ -0,0 +1,15 @@
+#Pong
+
+import turtle
+
+wn = turtle.Screen()
+wn.title("Pong")
+wn.bgcolor("black")
+wn.setup(width=800, height=600)
+wn.tracer(0)
+
+
+
+# Main game loop
+while True:
+    wn.update()
