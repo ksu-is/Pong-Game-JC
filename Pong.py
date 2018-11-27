3ee86ca87e2b66ee82894ee4f68858b51cb49c2b
jcruse2 <44611075+jcruse2@users.noreply.github.com>
2018-11-27 01:38:54 -0500

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -86,23 +86,31 @@ while True:
     ball.setx(ball.xcor() + ball.dx)
     ball.sety(ball.ycor() + ball.dy)
 
-	# Border checking
+    # Border checking
+    # Top and bottom
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-
-    if ball.ycor() < -290:
+            
+    elif ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
 
-    if ball.xcor() > 390:
+    # Left and right
+    if ball.xcor() > 350:
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
         ball.goto(0, 0)
         ball.dx *= -1
 
-    if ball.xcor() < -390:
+    elif ball.xcor() < -350:
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
         ball.goto(0, 0)
         ball.dx *= -1
-
+	
       # Paddle and ball collisions
     if ball.xcor() < -340 and ball.ycor() < paddle_a.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50:
         ball.dx *= -1 
